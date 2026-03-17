2286eb5d2ea50235ec866b05cbf70e6cdeec7259
Administrator <Administrator@A316-89>
2026-03-17 14:09:59 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2026_3_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test joe, tom, mary;
+
+            joe = new Test();
+            mary = new Test();
+
+            joe.Mid = 80;
+            joe.Final = 90;
+
+            mary.Mid = 60;
+            mary.Final = 66;
+
+            Console.WriteLine("Joe " + joe.GetAvg());
+
+            tom = joe;
+
+            Console.WriteLine("Tom " + tom.GetAvg());
+
+            joe.SetGrade(99, 100);
+
+            Console.WriteLine("Joe " + joe.GetAvg());
+        }
+    }
+}
